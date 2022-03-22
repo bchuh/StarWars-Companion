9be3baf18b89fc906887dcd6cf954ba32b981eb4
bchuh <zhuzengliangll@163.com>
2022-03-22 12:35:54 +0800

    Add Fronter Cam view

--- Fronter/mainwindow.cpp
+++ Fronter/mainwindow.cpp
@@ -1,32 +1,47 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QTime>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
+
     ui->setupUi(this);
-    cv::Mat m_imageShow;
-    cv::Mat image=cv::imread("C:/Users/YRY/Pictures/cat.png");
-    cv::resize(image,image,cv::Size(image.cols/4*4,image.rows/4*4));
-    //场景
     QGraphicsScene * m_scene;
     //画布
-    QGraphicsPixmapItem * m_imageItem;
     m_scene = new QGraphicsScene();
-    m_imageItem = new QGraphicsPixmapItem();
+    this->m_imageItem = new QGraphicsPixmapItem();
     //场景增加画布
     m_scene->addItem(m_imageItem);
-    //控件绑定场景
     ui->graphicsView->setScene(m_scene);
-    //记录绘制图片
-    //m_imageShow = image.clone();
-    cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
-    m_imageItem->setPixmap(QPixmap::fromImage(QImage((const unsigned char*)image.data, image.cols, image.rows, QImage::Format::Format_RGB888)));
-
     ui->graphicsView->show();
-}
+    this->cam=Camera::getInstance();
 
+}
+void MainWindow::run(){
+    cv::Mat image;
+    QTime dieTime;
+    while(true)
+    {
+        image=cam->nextFrame();
+        //cv::resize(image,image,cv::Size(image.cols/4*4,image.rows/4*4));
+        //场景
+
+        //控件绑定场景
+
+        //记录绘制图片
+        //m_imageShow = image.clone();
+        cv::cvtColor(image, image, cv::COLOR_BGR2RGB);
+        QImage  img2(image.data,image.cols,image.rows, image.step, QImage::Format_RGB888);
+        //m_imageItem->setPixmap(QPixmap::fromImage(QImage((const unsigned char*)image.data, image.cols, image.rows, QImage::Format::Format_RGB888)));
+        m_imageItem->setPixmap(QPixmap::fromImage(img2));
+        dieTime=QTime::currentTime().addMSecs(80);
+                while(QTime::currentTime()<dieTime)
+                    QCoreApplication::processEvents(QEventLoop::AllEvents,80);
+        //update();
+    }
+}
 MainWindow::~MainWindow()
 {
     delete ui;
--- Fronter/mainwindow.h
+++ Fronter/mainwindow.h
@@ -4,8 +4,14 @@
 #include <QMainWindow>
 #include <QGraphicsScene>
 #include <QGraphicsItem>
-#include <opencv.hpp>
-#include <imgproc\types_c.h>
+#include <opencv2\opencv.hpp>
+#include "C:\Users\zhuze\OneDrive - Macau University of Science and Technology\Bill\3th-2\Software_proj_manage\StarWars-Companion\Camera_module\camera_module.h"
+
+//连接cam模块步骤：1.项目右键, add existing directory, 选摄像头模块的文件夹
+//2. 把上面的include路径改为你电脑上的camera_module.h的绝对路径
+
+//#include <imgproc\types_c.h>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -18,8 +24,11 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void run();
 
 private:
     Ui::MainWindow *ui;
+    QGraphicsPixmapItem * m_imageItem;
+    Camera*cam;
 };
 #endif // MAINWINDOW_H
